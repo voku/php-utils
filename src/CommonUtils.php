5d770e0561291b0d329be0ed818d97ca8c9e3629
Minhao Zhang <minhao.zhang@qq.com>
2016-01-19 21:02:05 +0800

    + common utils, and canonical way to determine cli usage

---
+++ src/CommonUtils.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhao
+ * Date: 2016-01-19
+ * Time: 21:00
+ */
+
+namespace Oasis\Mlib\Utils;
+
+class CommonUtils
+{
+    public static function isRunningFromCommandLine()
+    {
+        return (
+            !isset($_SERVER['SERVER_SOFTWARE'])
+            && (
+                php_sapi_name() == 'cli'
+                || (is_numeric($_SERVER['argc']) && $_SERVER['argc'] > 0)
+            )
+        );
+    }
+}
